fa4d9ece9921ff00d586227e68262bd44eceb4e5
matheus fernandes <fernandesdesouza1914@gmail.com>
2024-05-10 07:16:07 -0300

    mudando para codigo para calculo de porcetagem

--- src/App.java
+++ src/App.java
@@ -4,18 +4,43 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("me diga um numero qualquer : ");
+        System.out.print("Digite seu salario : ");
 
-        int numero = sc.nextInt();
+        double salario = sc.nextDouble();
+        double aumento;
+        double novoSalario;
+        int porcetagem ;
 
-        double impaPar = numero % 2;
+        if (salario <= 280) {
+            aumento = (20 * salario) / 100;
+            porcetagem = 20;
+      
 
-        if (impaPar != 0) {
-            System.out.println("impar");
+        } else if (salario > 280 && salario < 700) {
+            aumento = (15 * salario) / 100;
+            porcetagem = 15;
+          
+
+        } else if (salario > 700 && salario < 1500) {
+            aumento = (10 * salario) / 100;
+            porcetagem = 10;
 
         } else {
-            System.out.println("par");
+            aumento = (5 * salario) / 100;
+            porcetagem = 5;
+         
+
         }
+
+        System.out.println("salario antes do reajuste : " + salario);
+
+        System.out.println("porcetagem de aumento  " + porcetagem);
+
+        System.out.println("valor do aumento " + aumento);
+
+        novoSalario = salario + aumento;
+
+        System.out.println("seu novo salario é " + novoSalario);
 
         sc.close();
 
